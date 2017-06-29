d118eef32a415335c98c8d1dcc5d500edffbf6cc
Valerie Burzynski <dev+vburzynski@bendyworks.com>
2017-06-29 13:28:31 -0500

    implement new node route

--- client/app/pods/nodes/new/route.js
+++ client/app/pods/nodes/new/route.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('node');
+  },
+  actions: {
+    saveNode(node) {
+      node.save().then(() => this.transitionTo('nodes'));
+    },
+    willTransition() {
+      this.controller.get('model').rollbackAttributes();
+    }
+  }
 });
