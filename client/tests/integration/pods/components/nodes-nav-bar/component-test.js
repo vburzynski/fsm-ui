250ce916c8d54dfb1b2e9472e3d52f0ac5f61127
Valerie Burzynski <dev+vburzynski@bendyworks.com>
2017-07-06 14:36:27 -0500

    Add formatting to pages

---
+++ client/tests/integration/pods/components/nodes-nav-bar/component-test.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+
+describe('Integration | Component | nodes nav bar', function() {
+  setupComponentTest('nodes-nav-bar', {
+    integration: true
+  });
+
+  it('renders', function() {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+    // Template block usage:
+    // this.render(hbs`
+    //   {{#nodes-nav-bar}}
+    //     template content
+    //   {{/nodes-nav-bar}}
+    // `);
+
+    this.render(hbs`{{nodes-nav-bar}}`);
+    expect(this.$()).to.have.length(1);
+  });
+});
